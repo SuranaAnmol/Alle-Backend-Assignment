461a54a4b60568a92b21e3979a5013ce8d7d2264
anmol surana <anmolsurana@MacBook-Air.local>
2025-03-12 01:30:14 +0530

    Defined the basic structure and the endpoints

---
+++ alle-assignment-project/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/gin-gonic/gin"
+    "github.com/SuranaAnmol/Alle-Backend-Assignment-Go/config"
+    "github.com/SuranaAnmol/Alle-Backend-Assignment-Go/routes"
+)
+
+func main() {
+    fmt.Println("🚀 Starting Task Management System...")
+
+    // Connect to MongoDB BEFORE doing anything else
+    config.ConnectDB()
+
+    router := gin.Default()
+    routes.SetupRoutes(router)
+
+    fmt.Println("🌍 Server running on http://localhost:8080")
+    router.Run(":8080") // Runs the server on port 8080
+}
